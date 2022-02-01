6c8ffcbe160e542c635127b01b30d0316e4a05f7
Julia Jespersdotter Högman <julia.jespersdotter@gmail.com>
2022-02-01 08:51:15 +0100

    Initial commit.

---
+++ node/test.js
@@ -0,0 +1,13 @@
+// // const a = "yoshi";
+// // console.log(a);
+
+// const greet = (a) => {
+//     console.log(`hello, ${a}`);
+// }
+
+// greet('mario');
+// greet('yoshi');
+
+// Global object
+
+console.log(global);
