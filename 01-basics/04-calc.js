48cd99140add6914b33fa81eadb7d53f97abbb1e
Julia Jespersdotter Högman <julia.jespersdotter@gmail.com>
2022-02-01 13:58:12 +0100

    Rounding numbers with Math with one decimal.

--- 01-basics/04-calc.js
+++ 01-basics/04-calc.js
@@ -8,6 +8,8 @@ console.log("geo:", geo);
 let radius = 4;
 
 let area = geo.area(radius);
+let approxArea = Math.round(area * 10) / 10;
 let circumference = geo.circumference(radius);
-console.log(`The area of a circle with radius ${radius} is: ~`, Math.round(area));
-console.log(`The circumference of a circle with radius ${radius} is: ~`, Math.round(circumference));
+let approxCircum = Math.round(circumference * 10) /10;
+console.log(`The area of a circle with radius ${radius} is: ~`, approxArea);
+console.log(`The circumference of a circle with radius ${radius} is: ~`, approxCircum);
