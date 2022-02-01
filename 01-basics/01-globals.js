8e714e3192c6f9c8782bdb29c5ee139d8b22db8d
Julia Jespersdotter Högman <julia.jespersdotter@gmail.com>
2022-02-01 11:00:08 +0100

    Changed file names, first node test.

---
+++ 01-basics/01-globals.js
@@ -0,0 +1,16 @@
+/**
+ * Globals
+ */
+
+console.log("Hello, world!");
+// console.log(global);
+
+setTimeout(() => {
+    console.log("I'll be back!");
+}, 3000);
+
+let counter = 0;
+setInterval(() => {
+    counter += 2;
+    console.log(`Hello after ${counter} seconds`);
+}, 2000);
