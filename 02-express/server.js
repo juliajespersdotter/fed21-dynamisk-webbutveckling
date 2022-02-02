05415697343f5960fd487f47d03ef95dfe4ce3af
Julia Jespersdotter Högman <julia.jespersdotter@gmail.com>
2022-02-02 14:23:07 +0100

    Send html files using path module.

--- 02-express/server.js
+++ 02-express/server.js
@@ -3,6 +3,7 @@
  */
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
@@ -11,8 +12,24 @@ app.get('/', (req, res) => {
     // req = information om den inkommande förfrågan
     // res = metoder för att skicka ett svar på förfrågan
     res.send('Hello World!');
+    console.log(req.method, req.url);
     console.log("Someone requested my root!");
 })
+
+// respond to GET request for `/nom` 
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname+'/pages/index.html'));
+})
+
+// respond to GET request for `/about` 
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname+'/pages/about.html'));
+})
+
+// respond to GET request for `/api/nom``
+app.get('/nom', (req, res) => {
+    res.sendFile(path.join(__dirname+'/pages/nom.html'));
+}) 
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
