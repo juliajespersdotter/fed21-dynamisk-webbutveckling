5c70eeedb0c6a4125d5634f04fe70e6dcd2ddd6c
Julia Jespersdotter Högman <julia.jespersdotter@gmail.com>
2022-02-02 12:00:14 +0100

    Installed nodemon.

--- 02-express/server.js
+++ 02-express/server.js
@@ -6,8 +6,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// When a GET request for `/` (http://localhost:3000/) is received, run this function
 app.get('/', (req, res) => {
+    // req = information om den inkommande förfrågan
+    // res = metoder för att skicka ett svar på förfrågan
     res.send('Hello World!');
+    console.log("Someone requested my root!");
 })
 
 app.listen(port, () => {
