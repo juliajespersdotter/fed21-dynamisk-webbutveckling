71a1eab66cfeefdd4b3a3af951220ef4617dfc13
Julia Jespersdotter Högman <julia.jespersdotter@gmail.com>
2022-02-02 10:59:14 +0100

    Started listening to port 3000.

---
+++ 02-express/server.js
@@ -0,0 +1,15 @@
+/**
+ * Creating an express server
+ */
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
