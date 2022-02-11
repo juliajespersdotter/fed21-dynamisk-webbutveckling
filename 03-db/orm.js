7c4f52f544b53debb659cd51391ed1d9628cf9be
Julia Jespersdotter Högman <julia.jespersdotter@gmail.com>
2022-02-11 15:57:22 +0100

    Created module for table pokemon friends.

--- 03-db/orm.js
+++ 03-db/orm.js
@@ -1,6 +1,49 @@
-const PokemonCards = require('./PokemonCards');
+const PokemonFriends = require('./PokemonFriends');
 
-PokemonCards.fetchAll().then((collection) => {
-    console.log(collection);
-    console.log(collection.toJSON());
+PokemonFriends.fetchAll().then((result) => {
+    console.log(result);
+    console.log(result.toJSON());
 });
+
+PokemonFriends.count().then((count) => {
+    console.log('Vi har nu ' + count + ' Pokemon vänner!');
+});
+
+/*
+for (let i = 1; i < 11; i++) {
+    const friend = {
+        name: 'name'+i,
+        email: 'name'+i+'@email.com'
+    };
+    let newFriend = new PokemonFriends(friend);
+    newFriend.save().then((res) => {
+    console.log(res);
+})
+}
+*/
+// Delete all 
+PokemonFriends.where("name", "LIKE", "name%").fetchAll().then((result) =>{
+    console.log(result.toJSON());
+    // Loopa igenom alla resultaten
+    result.forEach(model => {
+        model.destroy().then();
+    });
+})
+
+
+// PokemonFriends.where(friend).fetchAll().then((collection) => {
+//     console.log(collection.toJSON());
+// });
+
+/*
+const attribute = {
+    name: 'Skrelp',
+    hp: 50
+};
+
+let Skrelp = new PokemonFriends(attribute);
+Skrelp.save().then((res) => {
+    console.log(res);
+});
+*/
+// destroy() -- för att radera
