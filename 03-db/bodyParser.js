cfb6dd44c13366766782adf1e77e0adad5d81bba
Julia Jespersdotter Högman <julia.jespersdotter@gmail.com>
2022-02-11 09:49:13 +0100

    fileupload and bodyparse.

--- 03-db/bodyParser.js
+++ 03-db/bodyParser.js
@@ -3,6 +3,25 @@ const app = express();
 
 app.use(express.static('static'));
 
+app.get('/bodyParser', (req, res) => {
+    console.log('GET /bodyParser');
+    console.log(req.query); // Vid GET -- data i query
+    console.log('Name = ' + req.query.name);
+    console.log('Profile = ' + req.query.profile);
+    res.send('OK'); // Skicka tillbaka ett svar
+});
+
+const bodyParser = require('body-parser');
+const urlEncodedBodyParser = bodyParser.urlencoded( { extended: false } );
+
+app.post('/bodyParser', urlEncodedBodyParser, (req, res) => {
+    console.log(req.query); // Vid GET -- data i query
+    console.log(req.body);
+    console.log("Name = " + req.body.name);
+    console.log("Profile = " + req.body.profile);
+    res.send('OK');
+});
+
 app.listen(3000, () => {
     console.log('Server started at http://localhost:3000');
-})
+});
