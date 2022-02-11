8398c9b9cdb5eb894d6a64c49d0f5bcea162adf1
Julia Jespersdotter Högman <julia.jespersdotter@gmail.com>
2022-02-11 10:55:09 +0100

    Enabled uploading files.

--- 03-db/fileupload.js
+++ 03-db/fileupload.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const app = express();
+const multer = require('multer'); // Ladda in Multer
 
 app.use(express.static('static'));
+
+/*
+const urlencoded = express.urlencoded({ extended: false });
+const jsonencoder = express.json();
+*/
+
+// app.use(urlencoded);
+
+const storageObj = multer.diskStorage(
+    {
+        destination: (req, file, cb) => {
+            cb(null, 'uploads');
+        },
+        filename: (req, file, cb) => {
+            cb(null, file.originalname);
+        }
+    }
+);
+const multipartDataEncoder = multer({ storage: storageObj }); // Using multer object
+
+app.post('/upload', multipartDataEncoder.single('myFile'),  (req, res) =>{
+    console.log(req.file);
+    res.send(req.file); // send back file to client
+});
 
 app.listen(3000, () => {
     console.log('Server started at http://localhost:3000');
