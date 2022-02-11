73c05e9f8bab73b02731507ed512689743f4d958
Julia Jespersdotter Högman <julia.jespersdotter@gmail.com>
2022-02-11 14:50:09 +0100

    Using bookshelf to show pokemoncards table.

--- 03-db/knex.js
+++ 03-db/knex.js
@@ -19,3 +19,45 @@ connection.select().table('PokemonCards').then((result) => {
         console.log('Pokemon har namnet ' + r.name + ' med hp till ' + r.hp);
     })
 });
+
+/*
+connection.select('id', 'name').table('PokemonCards').then((result) => { // SELECT * FROM PokemonCards
+    console.log(result);
+});
+*/
+
+/*
+// SELECT id, name FROM PokemonCards WHERE id = 2
+connection.select('id', 'name', 'hp').where({id: 2}).orWhere({id: 4}).table('PokemonCards').then((result) => { 
+    console.log(result);
+});
+*/
+/*
+// SELECT id, name FROM PokemonCards WHERE id IN (2, 4, 6)
+connection.select('id', 'name', 'hp').whereIn('id', [2, 4, 6]).table('PokemonCards').then((result) => { 
+    console.log(result);
+
+});
+*/
+
+/*
+// SELECT * FROM PokemonCards WHERE id = 1
+connection('PokemonCards').where('id', 1).select().then((result) =>{
+    console.log(result);
+});
+*/
+
+/*
+connection('PokemonCards').insert({name: 'Nodemon', hp: 163}).finally((result) => {
+    console.log(result);
+});
+*/
+
+// "Fulhack" för att stänga server, alt använd nodemon
+/*
+const s = 1;
+setTimeout(
+    () => { process.exit(0); },
+        s*1000
+);
+*/
