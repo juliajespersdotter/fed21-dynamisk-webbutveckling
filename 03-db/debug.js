e7f8825a2b19aed24a2d40dd733461ee98c8992b
Julia Jespersdotter Högman <julia.jespersdotter@gmail.com>
2022-02-18 10:49:12 +0100

    npm scripts added.

---
+++ 03-db/debug.js
@@ -0,0 +1,13 @@
+const debug = require('debug');
+const express = require('express');
+const app = express();
+
+// Logger
+// logger:1
+// logger:2
+
+const testLogger = debug('logger');
+
+// testLogger('Detta är min testlogger');
+
+
