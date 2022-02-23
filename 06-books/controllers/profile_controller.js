076e3db5739a8d82c16fb03189f3fd1da968b854
Julia Jespersdotter Högman <julia.jespersdotter@gmail.com>
2022-02-23 15:20:05 +0100

    Added updateProfile and getBooks.

--- 06-books/controllers/profile_controller.js
+++ 06-books/controllers/profile_controller.js
@@ -26,21 +26,46 @@ const getProfile = async (req, res) => {
  * PUT /
  */
 const updateProfile = async (req, res) => {
-	res.status(405).send({
-		status: 'error',
-		message: 'This is a workshop'
-	});
-	
+	// make sure user exists
+	const user = await new models.User({ id: req.user.id }).fetch({ require: false });
+
+	// Checking after errors before updating user
+	const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(422).send({ status : "fail", data: errors.array() });
+    }
+    const validData = matchedData(req); 
+
+	try {
+		const updatedUser = await user.save(validData);
+		debug("Updated user successfully: %O", updatedUser);
+		res.send({
+			status: 'success',
+			data: {
+				updatedUser,
+			},
+		});
+	} catch (error) {
+		res.status(500).send({
+			status: 'error',
+			message: 'Exception thrown in database when updating a new user.',
+		});
+		throw error;
+	}
 }
+	
 
 const getBooks = async (req, res) => {
 	// somehow get the authenticated user's books
 	// and return it
+	const user_books = await new models.User({ id: req.user.id }).fetch({withRelated: ['books']});
 
-	res.status(405).send({
-		status: 'error',
-		message: 'This is a workshop'
-	});
+		res.send({
+			status: 'success',
+			data: {
+				books: user_books,
+			}
+		});
 }
 
 
--- 06-books/routes/profile.js
+++ 06-books/routes/profile.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const profileController = require('../controllers/profile_controller');
-// const profileValidationRules = require('../validation/user');
+const validUser = require('../validation/user')
 
 /** 
  * Get authenticated user's profile 
@@ -12,7 +12,7 @@ router.get('/', profileController.getProfile);
  * 
  * @todo Add validation rules
  */
-router.put('/', profileController.updateProfile);
+router.put('/', validUser.updateRules, profileController.updateProfile);
 
 
 /**
