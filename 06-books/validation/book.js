4756ccaaa4f2aee0c2d39e42f1e055ddd716c122
Julia Jespersdotter Högman <julia.jespersdotter@gmail.com>
2022-02-23 09:35:12 +0100

    Added author_id authentication rules.

--- 06-books/validation/book.js
+++ 06-books/validation/book.js
@@ -3,20 +3,35 @@
  */
 
  const { body } = require('express-validator');
+const models = require('../models');
 
  const createRules = [
-     body('title').exists().isLength( { min: 2 }),
-     body('isbn').exists().isLength({ min: 2 }),
-     body('pages').exists().isLength({ min: 1 }),
-     body('author_id').exists().isLength({ min: 1 }),
+     body('title').exists().isLength( { min: 4 }),
+     body('isbn').optional().isLength({ min: 10, max: 13 }),
+     body('pages').optional().isInt({ min: 1 }),
+     body('author_id').exists().custom(async value => {
+         const author = await new models.Author({ id: value }).fetch({ require: false });
+         if(!author) {
+             return Promise.reject(`Author with ID ${value} does not exist.`);
+         }
+
+         return Promise.resolve();
+     }),
  ];
  
  // allow only password, first_name, last_name to be updated, only optionally
  const updateRules = [
-     body('title').optional().isLength({ min: 2 }),
-     body('isbn').optional().isLength({ min: 2 }),
-     body('pages').optional().isLength({ min: 1 }),
-     body('author_id').optional().isLength({ min: 1 }),
+     body('title').optional().isLength({ min: 4 }),
+     body('isbn').optional().isLength({ min: 10, max: 13 }),
+     body('pages').optional().isInt({ min: 1 }),
+     body('author_id').optional().custom(async value => {
+        const author = await new models.Author({ id: value }).fetch({ require: false });
+        if(!author) {
+            return Promise.reject(`Author with ID ${value} does not exist.`);
+        }
+
+        return Promise.resolve();
+    }),
  ];
  
  module.exports = {
