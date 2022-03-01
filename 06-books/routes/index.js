01ec249085f5e306d64c7240eafe6ad901aeb93a
Julia Jespersdotter Högman <julia.jespersdotter@gmail.com>
2022-03-01 10:36:29 +0100

    Deleted user route and controller, installed jwt.

--- 06-books/routes/index.js
+++ 06-books/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middlewares/auth');
 const userValidationRules = require('../validation/user');
-const registerController = require('../controllers/register_controller');
+const registerController = require('../controllers/auth_controller');
 
 /* GET / */
 router.get('/', (req, res, next) => {
@@ -12,7 +12,6 @@ router.get('/', (req, res, next) => {
 router.use('/authors', require('./authors'));
 router.use('/books', require('./books'));
 router.use('/profile', auth.basic , require('./profile'));
-// router.use('/users', require('./users'));
 
 // register a new user
 router.post('/register', userValidationRules.createRules, registerController.register);
