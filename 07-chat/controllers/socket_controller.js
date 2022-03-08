7f4c745e1ec44c5b0d28cefd7146bcb3f5b44ef9
Julia Jespersdotter Högman <julia.jespersdotter@gmail.com>
2022-03-08 14:15:52 +0100

    Added li element for new messages for client and server.

--- 07-chat/controllers/socket_controller.js
+++ 07-chat/controllers/socket_controller.js
@@ -4,4 +4,20 @@
 
 const debug = require('debug')('chat:socket_controller');
 
-module.exports = {}
+
+
+module.exports = function(socket) {
+	debug('a new client has connected', socket.id);
+
+	socket.on('disconnect', function() {
+        debug(`Client ${this.id} disconnected :(`);
+    });
+
+    // listening to event chat:message from chat.js
+    socket.on('chat:message', function(message) {
+        debug('Someone said something:', message);
+
+        // emit `chat:message` event to everyone EXCEPT the sender
+        this.broadcast.emit('chat:message', message);
+    });
+}
--- 07-chat/public/assets/js/chat.js
+++ 07-chat/public/assets/js/chat.js
@@ -1,5 +1,26 @@
+const messagesEl = document.querySelector('#messages'); // ul element containing all messages 
 const messageForm = document.querySelector('#message-form');
 const messageEl = document.querySelector('#message');
+
+const socket = io();
+
+const addMessageToChat = message => {
+	const liEl = document.createElement('li');
+	liEl.innerText = message;
+
+	// append `li`element to `#messages`
+	messagesEl.appendChild(liEl);
+
+	//scroll `li` element into view
+	liEl.scrollIntoView();
+}
+
+// listen for incoming messages
+socket.on('chat:message', message => {
+	console.log("Someone said something", message);
+
+	addMessageToChat(message);
+})
 
 messageForm.addEventListener('submit', e => {
 	e.preventDefault();
@@ -8,5 +29,14 @@ messageForm.addEventListener('submit', e => {
 	if (!messageEl.value) {
 		return;
 	}
+
+	// send message to server
+	socket.emit('chat:message', messageEl.value);
+
+	addMessageToChat(messageEl.value);
+
+	// clear message input element
+	messageEl.value = '';
+	messageEl.focus();
 
 });
